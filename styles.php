7b74bd50bc856e0a2824ff153c73e559d0ea3648
Dhruv Lalwani <dhruvlalwani004@gmail.com>
2024-03-16 02:55:21 +0530

    assets folders rename

--- styles.php
+++ styles.php
@@ -1,8 +1,8 @@
 <link rel="icon" type="image/png" href="https://first1.us/assets/img/first-icon-32.png" />
 <!-- Custom CSS -->
-<link href="assets/css/styles.css?v=<?php echo APPVERSION;?>" rel="stylesheet">
-<link href="assets/css/all-styles.css?v=<?php echo APPVERSION;?>" rel="stylesheet">
-<link href="assets/css/responsive.css?v=<?php echo APPVERSION;?>" rel="stylesheet">
-<link href="assets/css/plugins/sweetalert.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="assets/css/pickadate.css" />
-<script src="assets/js/jquery.min.js"></script>
+<link href="../first1.us/assets/css/styles.css?v=<?php echo APPVERSION;?>" rel="stylesheet">
+<link href="../first1.us/assets/css/all-styles.css?v=<?php echo APPVERSION;?>" rel="stylesheet">
+<link href="../first1.us/assets/css/responsive.css?v=<?php echo APPVERSION;?>" rel="stylesheet">
+<link href="../first1.us/assets/css/plugins/sweetalert.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="../first1.us/assets/css/pickadate.css" />
+<script src="../first1.us/assets/js/jquery.min.js"></script>
