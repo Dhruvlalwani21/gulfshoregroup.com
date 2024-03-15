2f6e3fb7d473919eeebba102dd38782c8ecf9840
Dhruv Lalwani <dhruvlalwani004@gmail.com>
2024-03-16 03:10:01 +0530

    navbar

--- nav.php
+++ nav.php
@@ -1,97 +1,27 @@
-<nav id="navigation" class="navigation navigation-landscape">
-<div class="nav-header">
-    <?php
-    if($itsHome && $itsHome == 'Yes'){
-    ?>
-    <a class="nav-brand static-logo" href="/"><img src="<?php echo WEBLOGO;?>" class="logo" alt="" /></a>
-    <?php
-    }else{
-    ?>
-    <a class="nav-brand static-logo" href="/"><img src="<?php echo WEBLOGOALT;?>" class="logo" alt="" /></a>
-    <?php
-    }
-    ?>
-	<a class="nav-brand fixed-logo" href="/"><img src="<?php echo WEBLOGOALT;?>" class="logo" style="max-height: 40px;" alt="" /></a>
-	<div class="nav-toggle"></div>
-	<div class="mobile_nav">
-		<ul>
+  <nav>
+            <div className="brand"><h3>Gulfshore Group</h3></div>
+            <div className="tabs">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cities</li>
+                    <li>Buy</li>
+                    <li>Sell</li>
+                </ul>
+            </div>
+            <div className="icons">
             <?php
-            if(!isset($_SESSION['user_id'])){
-            ?>
-			<li><a href="javascript:;" data-toggle="modal" data-target="#login"><i class="fas fa-user-circle fa-lg"></i></a></li>
+        if(!isset($_SESSION['user_id'])){
+        ?>
+         <a href="logout.php"><span>Sign In</span></a>
             <?php
             }else{
             ?>
-			<li><a href="logout.php"><i class="fas fa-sign-out-alt fa-lg"></i></a></li>
+              <i class="fa-regular fa-user"></i>
+			<a href="logout.php"><i class="fas fa-sign-out-alt fa-lg"></i></a>
             <?php    
             }
             ?>
-		</ul>
-	</div>
-</div>
-<div class="nav-menus-wrapper" style="transition-property: right;">
-	<ul class="nav-menu">
-	
-		<li class=""><a href="/">Home</a></li>
-		<li><a href="buy">Buy</a></li>
-		<li><a href="sell">Sell</a></li>
-		<li><a href="mls-search">MLS Search</a></li>
-        <?php
-        $selCity = "SELECT * FROM cities WHERE city_id!='' ORDER BY name ASC";  
-        $cityRslts = mysqli_query($conn,$selCity);  
-        $noNavCity = mysqli_num_rows($cityRslts);
-        $cityData = $cityRslts->fetch_all(MYSQLI_ASSOC);
-                       
-        if($noNavCity>0){
-        ?>
-	    <li><a href="javascript:;" id="nav_top_city" onclick="slideSubMenu(this.id)">Top Cities<span class="submenu-indicator"></span></a>
-			<ul class="nav-dropdown nav-submenu"> 
-            <?php
-            foreach ($cityData as $row){
-            $nav_city_name = $row['name'];
-            $nav_city_slug = $row['slug'];
-            ?>
-    		<li><a href="city/<?php echo $nav_city_slug;?>"><?php echo $nav_city_name;?></a></li>
-            <?php
-            }
-            ?>
-			</ul>
-		</li>
-        <?php
-        }
-        
-        if(isset($_SESSION['user_id'])){
-        ?>
-		<li class=""><a href="settings">Settings</a></li>
-        <li><a href="favorites/1">Favorites <span class="fright">(<span id="favorites_counts"><?php echo $favorites;?></span>)</span></a></li>
-        <?php
-        }
-        ?>
-		<li><a href="about-us">About Us</a></li>
-		<li><a href="contact-us">Contact Us</a></li>
-		
-	</ul>
-	
-	<ul class="nav-menu nav-menu-social align-to-right">
-        <?php
-        if(!isset($_SESSION['user_id'])){
-        ?>
-		<li>
-			<a href="javascript:;" data-toggle="modal" data-target="#login">
-				<i class="fas fa-sign-in-alt mr-1"></i><span class="dn-lg">Sign In</span>
-			</a>
-		</li>
-        <?php
-        }else{
-        ?>
-		<li>
-			<a href="logout.php">
-				<i class="fas fa-sign-out-alt mr-1"></i><span class="dn-lg">Logout</span>
-			</a>
-		</li>
-        <?php   
-        }
-        ?>
-	</ul>
-</div>
-</nav>
+            </div>
+ </nav>
